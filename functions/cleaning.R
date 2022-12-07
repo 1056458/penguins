36be1c119343e83f1e31485cbcaf71f7fc1f7ef9
matildarousham <117754473+matildarousham@users.noreply.github.com>
2022-12-07 01:03:56 +0000

    Adding my files

---
+++ functions/cleaning.R
@@ -0,0 +1,22 @@
+
+#-----------------------------------------------------------------------------
+
+#Clean column names, remove empty rows, remove columns called comment and delta
+cleaning <- function(data_raw){
+  data_raw %>%
+    dplyr:: select(-c(Comments, Delta.15.N..o.oo., Delta.13.C..o.oo.)) %>%
+    janitor:: clean_names() %>%
+    janitor:: remove_empty(c("rows", "cols"))
+}
+
+#-----------------------------------------------------------------------------
+
+#Remove observations with missing data.
+remove_na <- function(data_clean){
+  data_clean %>%
+    dplyr:: filter(!is.na(flipper_length_mm)) %>%
+    dplyr:: filter(!is.na(body_mass_g)) %>%
+    dplyr:: filter(!is.na(sex))
+}
+
+#-----------------------------------------------------------------------------
---
+++ functions/libraries.R
@@ -0,0 +1,9 @@
+
+library(viridis)
+library(palmerpenguins)
+library(ggplot2)
+library(tidyverse)
+library(janitor)
+library(dplyr)
+library(ragg)
+library(svglite)
